d718205f95850e03e60e4d7311c3040c8b088736
Deepak Gunasekaran <deepakgsrn@gmail.com>
2021-10-16 22:10:44 +0530

    #2 completed exercise 1.1
    Completed exercise 1.1: course information, step1

---
+++ part1/course-information/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+
+const Header = ({course}) => <h1>{course}</h1>
+const Content = ({part, exercises}) => <p>{part} {exercises}</p>
+const Total = ({total}) => <p>Number of exercises {total}</p>;
+
+const App = () => {
+    const course = "Half Stack application development"
+    const part1 = "Fundamentals of React"
+    const exercises1 = 10
+    const part2 = "Using props to pass data"
+    const exercises2 = 7
+    const part3 = "State of a Component"
+    const exercises3 = 14
+    return (
+      <div>
+        <Header course={course} />
+        <Content part = {part1} exercises = {exercises1} />
+        <Content part = {part2} exercises = {exercises2} />
+        <Content part = {part3} exercises = {exercises3} />
+        <Total  total = {exercises1 + exercises2 + exercises3} />
+      </div>
+    );
+}
+
+export default App;
---
+++ part1/course-information/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Rendering the Main Component
+ReactDOM.render(<App />, document.getElementById('root'));
