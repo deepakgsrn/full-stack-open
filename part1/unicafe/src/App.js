5a4205ece20cc420b3db047a3c30a4c95df807b2
Deepak Gunasekaran <deepakgsrn@gmail.com>
2021-10-18 22:47:08 +0530

    #2 completed exercise 1.7
    Completed 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,6 +7,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const getTotalNoOfFeedback = () => good + neutral + bad
+  const totalNoOfFeedback = getTotalNoOfFeedback()
+  const averageFeedback = ((good - bad)/totalNoOfFeedback)>0 ? (good - bad)/totalNoOfFeedback : 0
+  const positivePercentage = (good/totalNoOfFeedback) > 0 ? (good/totalNoOfFeedback) * 100 : 0
+
+
     return (
       <div>
         <h1>Give Feedback</h1>
@@ -17,6 +23,9 @@ const App = () => {
         <p>Good {good}</p>
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
+        <p>Total Feedback {totalNoOfFeedback}</p>
+        <p>Average {averageFeedback}</p>
+        <p>Positive Percentage {positivePercentage} %</p>
       </div>
     );
 }
