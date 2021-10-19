9b537d7dcb4afe0922cb5f8604f9617441e7da9f
Deepak Gunasekaran <deepakgsrn@gmail.com>
2021-10-19 22:46:40 +0530

    #2 completed exercise 1.13
    Completed 1.13*: anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -5,24 +5,50 @@ const Button = ({text, onClick}) => <button onClick = {onClick}>{text}</button>
 
 const App = () => {
 
-    const anecdotes = [
-      'If it hurts, do it more often',
-      'Adding manpower to a late software project makes it later!',
-      'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-      'Premature optimization is the root of all evil.',
-      'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-      'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-    ]
+    const anecdotesData = {
+      //key is anecdote id & value is anecdote data
+      1: {
+        anecdote: 'If it hurts, do it more often',
+        vote: 0
+      },
+      2: {
+        anecdote: 'Adding manpower to a late software project makes it later!',
+        vote: 0
+      },
+      3: {
+        anecdote: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        vote: 0
+      },
+      4: {
+        anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        vote: 0
+      },
+      5: {
+        anecdote: 'Premature optimization is the root of all evil.',
+        vote: 0
+      },
+      6: {
+        anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        vote: 0
+      },
+      7: {
+        anecdote: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
+        vote: 0
+      }
+    }
 
-    const [selectedIndex, setSelectedIndex] = useState(() => Math.floor(Math.random() * anecdotes.length));
+    const [anecdotes, setAnecdotes] = useState(anecdotesData);
+
+    const noOfAcecdotes = Object.keys(anecdotesData).length;
+    const randomGenerator = () => Math.floor((Math.random() * noOfAcecdotes) + 1);
+
+    const [selectedIndex, setSelectedIndex] = useState(randomGenerator);
 
     const getRandomIndex = () =>{
-      let random = Math.floor(Math.random() * anecdotes.length);
+      let random = randomGenerator();
       do{
-        random = Math.floor(Math.random() * anecdotes.length);
+        random = randomGenerator();
       }while(selectedIndex === random);
-
       return random;
     }
 
@@ -30,10 +56,23 @@ const App = () => {
       setSelectedIndex(getRandomIndex());
     }
 
+    const handleVote = () => {
+      const newAnecdotes = {
+        ...anecdotes,
+        [selectedIndex] : {
+          ...anecdotes[selectedIndex],
+          vote: anecdotes[selectedIndex].vote + 1
+        }
+      }
+      setAnecdotes(newAnecdotes);
+    }
+
     return (
       <div>
         <h1>anecdote of the day !</h1>
-        <p>{anecdotes[selectedIndex]}</p>
+        <p>{`${anecdotes[selectedIndex]['anecdote']}`}</p>
+        <p>Has {`${anecdotes[selectedIndex]['vote']}`} votes</p>
+        <Button text="Vote" onClick = {handleVote} />
         <Button text="Next Anecdote" onClick = {handleNextAnecdote} />
       </div>
     );
